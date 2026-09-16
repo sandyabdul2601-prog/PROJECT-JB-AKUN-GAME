0874e1a80826a511c42e42762d4fccb04497408b
sandyabdul2601 <sandyabdul2601@gmail.com>
2026-09-16 08:52:26 +0700

    sandy

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/register', [AuthController::class, 'showRegister']);
-
-Route::post('/register', [AuthController::class, 'register']);
-
-
-Route::get('/login', [AuthController::class, 'showLogin']);
-
-Route::post('/login', [AuthController::class, 'login']);
-
-
-Route::post('/logout', [AuthController::class, 'logout'])
-    ->middleware('auth');
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware('auth');
